0bc4a76871917bea736382d19ca02f3bbce86c6c
Ronit Tetarbe <TetarbeR@cardiff.ac.uk>
2024-11-20 18:04:42 +0000

    Adding Files

---
+++ backend/routes/store.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+//get all store items
+router.get('/', (req,res) =>{
+
+} )
+
+//get one store item
+router.get('/:id', (req,res) =>{
+    
+} )
+
+//add store item
+router.post('/:id', (req,res) =>{
+    
+} )
+
+//delete store item
+router.delete('/:id', (req,res) =>{
+    
+} )
+
+// router.update('/', (req,res,next) =>{
+    
+// } )
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const storeRoutes = require("./routes/store")
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use('/', storeRoutes)
+
+// connect to db
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected to database");
+
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log("listening for requests on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
