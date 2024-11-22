dfeb5a0f8c64ae33491de586b7a721b4a30770db
Ronit Tetarbe <TetarbeR@cardiff.ac.uk>
2024-11-22 02:05:18 +0000

    Adding Routes

--- backend/routes/store.js
+++ backend/routes/store.js
@@ -1,26 +1,50 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const store = require("../model/storeModel");
 
 //get all store items
-router.get('/', (req,res) =>{
+router.get("/", async (req, res) => {
+  const storeitem = await store.find({}).sort({ createdAt: -1 });
 
-} )
+  res.status(200).json(storeitem);
+});
 
 //get one store item
-router.get('/:id', (req,res) =>{
-    
-} )
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such item." });
+  }
+
+  const storeitem = await store.findById(id);
+
+  if (!store) {
+    return res.status(404).json({ error: "No such item." });
+  }
+
+  res.status(200).json(storeitem);
+});
 
 //add store item
-router.post('/:id', (req,res) =>{
+// router.post("/:id", async (req, res) => {
     
-} )
+// });
 
 //delete store item
-router.delete('/:id', (req,res) =>{
-    
-} )
+router.delete("/:id", async(req, res) => {
+    const { id } = req.params
 
-// router.update('/', (req,res,next) =>{
-    
-// } )
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such item.'})
+  }
+
+  const storeitem = await store.findOneAndDelete({_id: id})
+
+  if(!workout) {
+    return res.status(400).json({error: 'No such item.'})
+  }
+
+  res.status(200).json(storeitem)
+});
